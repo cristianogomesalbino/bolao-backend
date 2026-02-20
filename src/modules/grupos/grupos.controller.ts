6fba7b74a3d83d7e02a3d77eacb0589b66283dca
Cristiano Gomes Albino <69982937+cristianogomesalbino@users.noreply.github.com>
2026-02-20 00:24:15 -0300

    [Swagger] - Implementado swagger no projeto

--- src/modules/grupos/grupos.controller.ts
+++ src/modules/grupos/grupos.controller.ts
@@ -4,31 +4,49 @@ import { CreateGrupoDto } from './dto/create-grupo.dto';
 import { UpdateGrupoDto } from './dto/update-grupo.dto';
 import { UpdateStatusGrupoDto } from './dto/update-status-grupo.dto';
 import { ParseUUIDCustomPipe } from '../../common/pipes/parse-uuid-custom.pipe';
+import { ApiTags, ApiOperation, ApiResponse, ApiBadRequestResponse, ApiNotFoundResponse,} from '@nestjs/swagger';
 
+@ApiTags('Grupos')
 @Controller('grupos')
 export class GruposController {
   constructor(private readonly gruposService: GruposService) {}
 
+  @ApiOperation({ summary: 'Criar um novo grupo' })
+  @ApiResponse({ status: 201, description: 'Grupo criado com sucesso.' })
+  @ApiBadRequestResponse({ description: 'Erro de validação.' })
+  @ApiNotFoundResponse({ description: 'Temporada ou administrador não encontrado.' })
   @Post()
   create(@Body() createGrupoDto: CreateGrupoDto) {
     return this.gruposService.create(createGrupoDto);
   }
 
+  @ApiOperation({ summary: 'Listar todos os grupos ativos' })
+  @ApiResponse({ status: 200, description: 'Lista de grupos retornada com sucesso.' })
   @Get()
   findAll() {
     return this.gruposService.findAll();
   }
 
+  @ApiOperation({ summary: 'Listar todos os grupos ativos' })
+  @ApiResponse({ status: 200, description: 'Lista de grupos retornada com sucesso.' })
+  @ApiNotFoundResponse({ description: 'Grupo não encontrado.' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.gruposService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'Buscar grupo por ID' })
+  @ApiResponse({ status: 200, description: 'Grupo encontrado.' })
+  @ApiNotFoundResponse({ description: 'Grupo não encontrado.' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateGrupoDto: UpdateGrupoDto) {
     return this.gruposService.update(id, updateGrupoDto);
   }
 
+  @ApiOperation({ summary: 'Alterar status (ativo/inativo) do grupo' })
+  @ApiResponse({ status: 200, description: 'Status atualizado com sucesso.' })
+  @ApiBadRequestResponse({ description: 'Erro de validação.' })
+  @ApiNotFoundResponse({ description: 'Grupo não encontrado.' })
   @Patch(':id/status')
   updateStatus(
     @Param('id', new ParseUUIDCustomPipe('id'))
@@ -38,6 +56,10 @@ export class GruposController {
     return this.gruposService.updateStatus(id, dto);
   }
 
+  @ApiOperation({ summary: 'Exclui um grupo inativo' })
+  @ApiResponse({ status: 200, description: 'Grupo excluído com sucesso.' })
+  @ApiBadRequestResponse({ description: 'Erro de validação.' })
+  @ApiNotFoundResponse({ description: 'Grupo não encontrado.' })
   @Delete(':id')
   remove(
     @Param('id', new ParseUUIDCustomPipe('id'))
--- src/modules/temporadas/temporadas.controller.ts
+++ src/modules/temporadas/temporadas.controller.ts
@@ -2,16 +2,24 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { TemporadasService } from './temporadas.service';
 import { CreateTemporadaDto } from './dto/create-temporada.dto';
 import { UpdateTemporadaDto } from './dto/update-temporada.dto';
+import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
 
+@ApiTags('Temporadas')
 @Controller('temporadas')
 export class TemporadasController {
   constructor(private readonly temporadasService: TemporadasService) {}
 
+  @ApiOperation({ summary: 'Cria uma nova temporada' })
+  @ApiBody({ type: CreateTemporadaDto })
+  @ApiResponse({ status: 201, description: 'Temporada criada com sucesso' })
+  @ApiResponse({ status: 400, description: 'Dados inválidos (ano ou campeonatoId)' })
   @Post()
   create(@Body() createTemporadaDto: CreateTemporadaDto) {
     return this.temporadasService.create(createTemporadaDto);
   }
 
+  @ApiOperation({ summary: 'Lista todas as temporadas' })
+  @ApiResponse({ status: 200, description: 'Lista de temporadas' })
   @Get()
   findAll() {
     return this.temporadasService.findAll();
