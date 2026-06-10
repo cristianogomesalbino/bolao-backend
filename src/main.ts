912f16969ce08d6491b877458f652066fd5578b5
Cristiano Albino <cristianogomesalbino@gmail.com>
2026-06-10 00:26:35 -0300

    [login] - Ajuste para login

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3003',
-      'https://bolao-frontend-beige.vercel.app',
-      'https://bolao-app-beige.vercel.app',
-    ],
+    origin: true,
     credentials: true,
   });
 
